2e91f0df0dddcff07a9afcb571583559b0d054e6
ajshedivy <ajshedivyaj@gmail.com>
2024-04-19 15:37:13 -0500

    rename stuff

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TAG=$(python -c 'from python_sc.version import VERSION; print("v" + VERSION)')
+TAG=$(python -c 'from python_wsdb.version import VERSION; print("v" + VERSION)')
 
 read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
 
