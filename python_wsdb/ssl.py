382c3bc3b1bb561c7ad3d92eba0cc029e03a944e
ajshedivy <ajshedivyaj@gmail.com>
2024-04-19 15:31:37 -0500

    add more tests

---
+++ python_wsdb/ssl.py
@@ -0,0 +1,19 @@
+import socket
+import ssl
+
+
+from python_wsdb import DaemonServer
+
+
+def get_certificate(creds: DaemonServer) -> (bytes | None):
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+    context.cert_reqs = ssl.CERT_NONE 
+
+    if creds.ca:
+        context.load_verify_locations(cadata=creds.ca if isinstance(creds.ca, str) else creds.ca.decode())
+    with socket.create_connection((creds.host, creds.port)) as sock:
+        with context.wrap_socket(sock, server_hostname=creds.host) as ssock:
+            return ssock.getpeercert(binary_form=True)
+        
