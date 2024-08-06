8abafc6df7a28ba78ae6f5bfa5f3beb104625541
Adam Shedivy <ajshedivyaj@gmail.com>
2024-08-06 16:43:29 -0500

    add check to python test

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -3,14 +3,22 @@ import os
 from python_wsdb.client.sql_job import SQLJob
 from python_wsdb.types import DaemonServer
 
+# Fetch environment variables
+server = os.getenv("VITE_SERVER")
+user = os.getenv("VITE_DB_USER")
+password = os.getenv("VITE_DB_PASS")
+
+# Check if environment variables are set
+if not server or not user or not password:
+    raise ValueError("One or more environment variables are missing.")
+
 creds = DaemonServer(
-    host=os.getenv("VITE_SERVER"),
+    host=server,
     port=8085,
-    user=os.getenv("VITE_DB_USER"),
-    password=os.getenv("VITE_DB_PASS"),
+    user=user,
+    password=password,
     ignoreUnauthorized=True,
 )
-
 
 def test_simple():
     job = SQLJob()
