e9df67e9422b384f98b7e4bbf73f26554e725d8b
Irfan Sharif <IrfanSharif@ibm.com>
2025-09-19 13:58:47 -0400

    Platform agnostic kerberos token generation
    Signed-off-by: Irfan Sharif <IrfanSharif@ibm.com>

--- mapepire_python/authentication/kerberosTokenProvider.py
+++ mapepire_python/authentication/kerberosTokenProvider.py
@@ -1,8 +1,23 @@
+import subprocess
 import base64
 import os
 import time
+import platform
+
 from typing import Optional
-import gssapi
+
+try:
+    import gssapi
+except ImportError:
+    gssapi = None
+
+# For Windows SSPI token generation:
+if platform.system() == "Windows":
+    try:
+        from kerberos_sspi import ClientAuth # type: ignore
+    except ImportError:
+        ClientAuth = None
+
 
 class KerberosTokenProvider:
     def __init__(
@@ -35,40 +50,62 @@ class KerberosTokenProvider:
 
     def _is_expired(self) -> bool:
         return time.time() >= self._token_expiry
-
+    
     def _refresh_token(self):
-        os.environ['KRB5_CONFIG'] = self.krb5_path
+        if platform.system() == "Windows":
+            self._refresh_token_windows()
+        else:
+            self._refresh_token_unix()
+
+
+    def _refresh_token_windows(self):
+        # Prefer using kerberos-sspi if available
+        if ClientAuth is None:
+            raise RuntimeError(
+                "kerberos-sspi package not installed, cannot generate Kerberos token on Windows"
+            )
+
+        service_spn = f"krbsvr400/{self.host}"
+        auth = ClientAuth(service_spn)
+        token = auth.authorize(None)
+
+        ticket_b64 = base64.b64encode(token).decode("utf-8")
+        self._token = "_KERBEROSAUTH_" + ticket_b64
+        self._token_expiry = time.time() + self.token_lifetime
+        self._token_used = False
+    
+    def _refresh_token_unix(self):
+        if gssapi is None:
+            raise RuntimeError("gssapi module not installed, cannot generate Kerberos token on Unix")
+
+        os.environ["KRB5_CONFIG"] = self.krb5_path
         if self.ticket_cache:
-            os.environ['KRB5CCNAME'] = self.ticket_cache
+            os.environ["KRB5CCNAME"] = self.ticket_cache
 
         mech = (
             gssapi.OID.from_int_seq("1.2.840.113554.1.2.2")
             if self.krb5_mech is None
             else gssapi.OID.from_int_seq(self.krb5_mech)
         )
+
         user_name = gssapi.Name(
             f"{self.realm_user}@{self.realm}", name_type=gssapi.NameType.user
         )
-        cred = gssapi.Credentials(name=user_name, usage='initiate', mechs=[mech])
+        cred = gssapi.Credentials(name=user_name, usage="initiate", mechs=[mech])
         server_name = gssapi.Name(
             f"krbsvr400@{self.host}", name_type=gssapi.NameType.hostbased_service
         )
-        ctx = gssapi.SecurityContext(name=server_name, mech=mech, creds=cred, usage='initiate')
+        ctx = gssapi.SecurityContext(name=server_name, mech=mech, creds=cred, usage="initiate")
 
-        token = ctx.step(b'')
+        token = ctx.step(b"")
         ticket_b64 = base64.b64encode(token).decode("utf-8")
         self._token = "_KERBEROSAUTH_" + ticket_b64
 
-        try:
-            lifetime = cred.lifetime
-            # Some implementations use GSS_C_INDEFINITE = 0xFFFFFFFF (or -1) to indicate indefinite lifetime
-            indefinite_lifetimes = [0xFFFFFFFF, -1]
-            if lifetime is None or lifetime in indefinite_lifetimes:
-                self._token_expiry = time.time() + self.token_lifetime
-            else:
-                self._token_expiry = time.time() + lifetime
-        except Exception:
+        lifetime = cred.lifetime
+        indefinite_lifetimes = [0xFFFFFFFF, -1]
+        if lifetime is None or lifetime in indefinite_lifetimes:
             self._token_expiry = time.time() + self.token_lifetime
-        
-        self._token_expiry = time.time() + self.token_lifetime
+        else:
+            self._token_expiry = time.time() + lifetime
+
         self._token_used = False
